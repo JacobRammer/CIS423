e88d5e68e34b669c34b71e99972b71fd23c390ac
jacobrammer <jrammer101@gmail.com>
2022-01-30 18:59:06 -0800

    adding find_random_state

--- library.py
+++ library.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer
+from sklearn.metrics import f1_score
 
 
 # This class maps values in a column, numeric or categorical.
@@ -220,3 +221,20 @@ class KNNTransformer(BaseEstimator, TransformerMixin):
 
   def fit_transform(self, x, x2=None):
     return self.transform(x)
+
+
+def find_random_state(df, labels, n=200):
+  # idx = np.array(abs(var - rs_value)).argmin()
+  error_list = []
+  for i in range(1, n):
+    x_train, x_test, y_train, y_test = train_test_split(df, labels, test_size=0.2, shuffle=True,
+                                                    random_state=i, stratify=labels)
+    model.fit(x_train, y_train)
+    x_train_pred = model.predict(x_train)  # predict against training set
+    x_test_pred = model.predict(x_test)  # predict against test set
+    train_error = f1_score(y_train, x_train_pred)  # how bad did we do with prediction on training data?
+    test_error = f1_score(y_test, x_test_pred) # how bad did we do with prediction on test data?
+    error_list.append(test_error / train_error) # take the ratio
+  
+  rs_value = sum(error_list)/len(error_list)
+  return np.array(abs(error_list - rs_value)).argmin()
