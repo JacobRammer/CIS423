a9d43a59563792ffd83ca4347bb14b71865d7f6b
jacobrammer <jrammer101@gmail.com>
2022-02-17 10:18:55 -0800

    chapter 11 imports

--- library.py
+++ library.py
@@ -3,8 +3,11 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingGridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 
 
 
